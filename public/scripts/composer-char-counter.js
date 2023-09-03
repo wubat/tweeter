9cb6baab424861001e8b1807a3fd4fd57ea9bc85
james <jameswu000@gmail.com>
2023-09-02 23:18:38 -0600

    added composer char counter function and interactive character counter that turns red if overLimit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  console.log('document loaded!!!')
+})
+
+const textarea = document.getElementById("tweet-text")
+
+$(textarea).on("keypress", function() {
+  let characterCounter = $(this).closest('.tweet-input').next('.submit-counter').find('output')
+  let tweetCharsLeft = 140 - $(this).val().length
+
+  if (tweetCharsLeft < 0) {
+    characterCounter.addClass('no-chars-left')
+  } else if (tweetCharsLeft >= 0) {
+    characterCounter.removeClass('no-chars-left')
+  }
+
+  characterCounter.text(tweetCharsLeft)
+  
+})
+
+
